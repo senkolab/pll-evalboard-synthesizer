358ba26a799ff09bf14cf7913abd5a07edfa9b03
Gilbert Shih <ldes89150@gmail.com>
2018-03-09 16:00:14 -0500

    implement socket connection for GUI client

--- src/GUI client/client_GUI.py
+++ src/GUI client/client_GUI.py
@@ -2,21 +2,45 @@ from PyQt5.QtWidgets import QFrame, QApplication, QWidget, QMainWindow
 import sys
 from mainwindow import Ui_MainWindow
 import json
+import zmq
 
 class client_GUI(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super(client_GUI, self).__init__()
         self.setupUi(self)
+        
+        self.context = zmq.Context()
+        self.socket = self.context.socket(zmq.REQ)
+        
         self.pushButton_save.clicked.connect(self.save_config)
         self.pushButton_load.clicked.connect(self.load_config)
+        self.pushButton_connect.clicked.connect(self.connect)
+        self.pushButton_set_device.clicked.connect(self.set_device)
+        self.dict_of_channel_widgets={
+            "EOM1": self.Channel_Widget_EOM_1,
+            "EOM2": self.Channel_Widget_EOM_2,
+            "EOM3": self.Channel_Widget_EOM_3,
+            "AOM1": self.Channel_Widget_AOM_1,
+            "AOM2": self.Channel_Widget_AOM_2,
+
+        }
+    def connect(self):
+        self.socket.connect("tcp://" + self.lineEdit_address.text())
+
+    def set_device(self):
+        data = []
+        for ch_widget in self.dict_of_channel_widgets.values():
+            if ch_widget.is_configuring_channel():
+                data.append(ch_widget.get_data())
+        print(data)
+        self.socket.send_json(data)
+        self.verbose("[REPLY] "+self.socket.recv_string())
 
     def save_config(self):
         config = dict()
-        config["EOM1"] = self.Channel_Widget_EOM_1.get_form()
-        config["EOM2"] = self.Channel_Widget_EOM_2.get_form()
-        config["EOM3"] = self.Channel_Widget_EOM_3.get_form()
-        config["AOM1"] = self.Channel_Widget_AOM_1.get_form()
-        config["AOM2"] = self.Channel_Widget_AOM_2.get_form()
+        for key, ch_widget in self.dict_of_channel_widgets.items():
+            config[key] = ch_widget.get_form()
+ 
         config["address"] = self.lineEdit_address.text()
 
         with open('config.json', 'w') as outfile:
@@ -31,11 +55,9 @@ class client_GUI(QMainWindow, Ui_MainWindow):
             self.verbose("[IOError] Cannot load configuration file!")
             return
 
-        self.Channel_Widget_EOM_1.set_form(config["EOM1"])
-        self.Channel_Widget_EOM_2.set_form(config["EOM2"])
-        self.Channel_Widget_EOM_3.set_form(config["EOM3"])
-        self.Channel_Widget_AOM_1.set_form(config["AOM1"])
-        self.Channel_Widget_AOM_2.set_form(config["AOM2"])
+        for key, ch_widget in self.dict_of_channel_widgets.items():
+            ch_widget.set_form(config[key])
+ 
         self.lineEdit_address.setText(config["address"])
         self.verbose("[MSG] Load Configuration successfully.")
         
